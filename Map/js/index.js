677dcf4d65df1538d568676b182cee67d39d0204
Topper Wonghart <TopperWH@Toppers-MacBook-Pro-2.local>
2017-12-16 13:25:19 -0800

    Map

---
+++ Map/js/index.js
@@ -0,0 +1,14 @@
+function initMap() {
+  var myLatLng = {lat: 37.786261, lng: -122.399533};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
